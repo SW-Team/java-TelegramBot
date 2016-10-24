746dd765b85c36333c73844777010995e6760898
milk0417 <milk5590@naver.com>
2016-10-24 11:47:01 +0900

    reply_markup 추가

--- src/milk/telegram/method/sender/TextMessageSender.java
+++ src/milk/telegram/method/sender/TextMessageSender.java
@@ -3,6 +3,7 @@ package milk.telegram.method.sender;
 import milk.telegram.bot.TelegramBot;
 import milk.telegram.type.message.Message;
 import milk.telegram.type.message.TextMessage;
+import milk.telegram.type.reply.ReplyMarkup;
 import org.json.JSONObject;
 
 public class TextMessageSender extends Sender{
@@ -54,6 +55,11 @@ public class TextMessageSender extends Sender{
     }
 
     @Override
+    public TextMessageSender setReplyMarkup(ReplyMarkup reply_markup){
+        return (TextMessageSender) super.setReplyMarkup(reply_markup);
+    }
+
+    @Override
     public TextMessageSender setDisableNotification(boolean value){
         return (TextMessageSender) super.setDisableNotification(value);
     }
@@ -66,6 +72,7 @@ public class TextMessageSender extends Sender{
         object.put("disable_web_page_preview", disable_web_page_preview);
 
         if(parse_mode != null) object.put("parse_mode", parse_mode);
+        if(reply_markup != null) object.put("reply_markup", reply_markup);
         if(message_id != -1) object.put("reply_to_message_id", message_id);
 
         return (TextMessage) Message.create(bot.updateResponse("sendMessage", object));
